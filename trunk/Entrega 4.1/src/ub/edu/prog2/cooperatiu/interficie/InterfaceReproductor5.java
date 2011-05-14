f4f418469dace1e6987bc9b81b29ac3cb172a372
yojmarinv@gmail.com <yojmarinv@gmail.com@5c073122-c9cb-1daa-3b90-16602da88f1d>
2011-05-14 12:52:09 +0000

    He hecho un par de cambios, ya me diras si los ves

---
+++ trunk/Entrega 4.1/src/ub/edu/prog2/cooperatiu/interficie/InterfaceReproductor5.java
@@ -0,0 +1,16 @@
+
+package ub.edu.prog2.cooperatiu.interficie;
+
+/**
+ * Aquesta interficie descriura tots els metodes als que crida la clase
+ *      Reproductor5 al paquet MarinVegaJuan.vista
+ * y
+ *      sera implementat per la clase BridgeReproductor5
+ * 
+ * @author Dennis Quitaquis , Juan Marin
+ */
+public interface InterfaceReproductor5{
+    
+    // A pensar y programar!!
+    
+}
